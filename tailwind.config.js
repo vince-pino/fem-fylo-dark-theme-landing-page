42bba38822da468436ebd7943c22410f3b22c665
Vince Cedie Pino <vincecediepino@gmail.com>
2024-10-02 20:27:26 +0700

    Add required fonts, add fontawsome script

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        "open-sans": ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "dark-blue-intro": "hsl(217, 28%, 15%)",
+        "dark-blue-main": "hsl(218, 28%, 13%)",
+        "dark-blue-footer": "hsl(216, 53%, 9%)",
+        "dark-blue-testimonial": "hsl(219, 30%, 18%)",
+        cyan: "hsl(176, 68%, 64%)",
+        blue: "hsl(198, 60%, 50%)",
+        "light-red-error": "hsl(0, 100%, 63%)",
+      },
+    },
   },
   plugins: [],
 };
